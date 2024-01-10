618a47145b87b15627c8224c5c5b51d3ab1f2ba3
akaashkasi <akaashkasi@gmail.com>
2024-01-10 17:28:52 -0600

    Added header commits

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.js
+import Header from './components/Header'; // Ensure this path is correct based on your file structure
+import './App.css'; // Your existing App.css file
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Rest of your app components */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './css/Header.css';
+import logo from '../assets/images/logo.png';
+
+const Header = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="menu-logo-container">
+        {/* Toggle button for sidebar */}
+        <button className="sidebar-toggle" onClick={toggleSidebar}>
+          <span
+            className={`hamburger-icon ${isSidebarOpen ? 'open' : ''}`}
+          ></span>
+        </button>
+        <div className="logo">
+          <img src={logo} alt="gymbuddy logo" /> gymbuddy
+        </div>
+      </div>
+
+      {/* Sidebar navigation */}
+      <nav className={`navigation ${isSidebarOpen ? 'show' : ''}`}>
+        {/* Close button inside sidebar for mobile */}
+        <button className="close-sidebar" onClick={toggleSidebar}>
+          &times;
+        </button>
+        <a href="#features">Features</a>
+        <a href="#about-us">About Us</a>
+        <a href="#contact">Contact</a>
+      </nav>
+
+      {/* Download button */}
+      <button className="download-button">
+        Download <span className="star">★</span>
+      </button>
+    </header>
+  );
+};
+
+export default Header;
